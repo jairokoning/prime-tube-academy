1e533bbaea7c2960be3094551a1a592f6ab2799c
Jairo Koning <jairo.koning@gmail.com>
2024-08-15 22:53:27 -0300

    feat[#3]: add course repository interface

---
+++ apps/backend/src/@core/application/repositories/CourseRepository.ts
@@ -0,0 +1,6 @@
+import Course from 'src/@core/domain/entities/course';
+
+export default interface CourseRepository {
+  save(course: Course): Promise<void>;
+  getById(courseId: string): Promise<Course>;
+}
