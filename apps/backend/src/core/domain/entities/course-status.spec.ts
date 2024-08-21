16337a352f6940219421439c609e0d1ae240a391
Jairo Koning <jairo.koning@gmail.com>
2024-08-21 18:48:54 -0300

    test[#3]: add course-status unit test

---
+++ apps/backend/src/core/domain/entities/course-status.spec.ts
@@ -0,0 +1,26 @@
+import Course from './course';
+
+describe('Course Status Units Test', () => {
+  it('should change course status', () => {
+    const input = {
+      title: 'Learn NestJS – Complete Course',
+      description:
+        'Learn NestJS in this comprehensive course for beginners. NestJS is a framework for building efficient, scalable Node.js web applications.',
+      url: 'https://www.youtube.com/watch?v=sFnAHC9lLaw',
+      year: 2024,
+      channel: 'freeCodeCamp.org',
+      tags: [{ tagId: '745a1869-9c90-4ed9-971e-a738e41adeb4', label: 'nest' }],
+      added_by: '745a1869-9c90-4ed9-971e-a738e41adeb4',
+    };
+    const course = Course.add(
+      input.title,
+      input.description,
+      input.url,
+      input.year,
+      input.channel,
+      input.tags,
+      input.added_by,
+    );
+    expect(course.getStatus()).toBe('PENDING');
+  });
+});
